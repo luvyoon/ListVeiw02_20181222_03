aeccc0f1d694f166a0e819e90f0cbf34ba26e132
Hottory <gkffndnl123@naver.com>
2018-12-22 13:38:47 +0900

    Initial commit

---
+++ app/src/main/java/kr/tjeit/listveiw02_20181222_03/MainActivity.java
@@ -0,0 +1,34 @@
+package kr.tjeit.listveiw02_20181222_03;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends  BaseActivity {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        bindViews();
+        setupEvent();
+        setValues();
+
+    }
+
+    @Override
+    public void setupEvent() {
+
+    }
+
+    @Override
+    public void setValues() {
+
+    }
+
+    @Override
+    public void bindViews() {
+
+    }
+}
