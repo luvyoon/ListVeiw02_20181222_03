ae02c84bf43deb2aa91d83f8fa56cda02385592c
Hottory <gkffndnl123@naver.com>
2018-12-22 14:19:58 +0900

    스토어 목록을 보여주는 사용할 xml작성

---
+++ app/src/main/java/kr/tjeit/listveiw02_20181222_03/adapters/StoreAdapter.java
@@ -0,0 +1,23 @@
+package kr.tjeit.listveiw02_20181222_03.adapters;
+
+import android.content.Context;
+import android.widget.ArrayAdapter;
+
+import java.util.List;
+
+import kr.tjeit.listveiw02_20181222_03.R;
+
+public class StoreAdapter extends ArrayAdapter<StoreAdapter> {
+
+    public StoreAdapter(Context mContext, List<StoreAdapter> mList){
+
+        super(mContext,R.layout.);
+
+
+
+
+    }
+
+
+
+}
