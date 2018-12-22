c32a54cfc7232e16106472e99a6adcb32542ac61
Hottory <gkffndnl123@naver.com>
2018-12-22 15:44:31 +0900

    가게 상세 화면 xml 작성

---
+++ app/src/main/java/kr/tjeit/listveiw02_20181222_03/StoreDetailActivity.java
@@ -0,0 +1,32 @@
+package kr.tjeit.listveiw02_20181222_03;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class StoreDetailActivity extends BaseActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_store_detail);
+
+        bindViews();
+        setupEvent();
+        setValues();
+    }
+
+    @Override
+    public void setupEvent() {
+
+    }
+
+    @Override
+    public void setValues() {
+
+    }
+
+    @Override
+    public void bindViews() {
+
+    }
+}
