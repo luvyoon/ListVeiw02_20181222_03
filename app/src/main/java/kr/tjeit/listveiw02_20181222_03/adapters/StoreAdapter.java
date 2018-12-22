ad78156a8d4892869167a8202ba56c50db4ea50c
Hottory <gkffndnl123@naver.com>
2018-12-22 14:59:50 +0900

    어댑터 클래스 작성 완료

--- app/src/main/java/kr/tjeit/listveiw02_20181222_03/adapters/StoreAdapter.java
+++ app/src/main/java/kr/tjeit/listveiw02_20181222_03/adapters/StoreAdapter.java
@@ -2,7 +2,11 @@ package kr.tjeit.listveiw02_20181222_03.adapters;
 
 import android.content.Context;
 import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import java.util.List;
 
@@ -20,17 +24,42 @@ public class StoreAdapter extends ArrayAdapter<StoreAdapter> {
         super(mContext,R.layout.store_list_item,mList);
 
         this.mContext=mContext;
-        this.mList =mList;
+        this.mList = mList;
         this.inf = LayoutInflater.from(mContext);
-
-
 
 
     }
 
 
+    @Override
+    public View getView(int position,  View convertView, ViewGroup parent) {
+        View row = convertView;
+
+        if(row == null){
+            row = inf.inflate(R.layout.store_list_item, null);
+
+        }
+        TextView storeNameTxt=row.findViewById(R.id.storeNameTxt);
+
+        TextView storeAddressTxT= row.findViewById(R.id.storeAddressTxT);
+        TextView StoreBtn= row.findViewById(R.id.StoreBtn);
+
+        Store data = mList.get(position);
+
+        storeNameTxt.setText(data.getName());
+        storeAddressTxT.setText(data.getAddress());
+        
+        StoreBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                Toast.makeText(mContext, "", Toast.LENGTH_SHORT).show();
+                
+            }
+        });
 
 
 
-
+        return row;
+    }
 }
